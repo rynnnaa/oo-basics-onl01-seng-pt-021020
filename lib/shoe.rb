1df09cf4df85b7559618525d35cd1f6b6e33f4bd
Ryna Truong <rynatruong@outlook.com>
2020-02-29 05:53:26 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,16 +1,11 @@
 class Shoe
-  attr_accessor :brand, :color, :size
-  attr_reader :material
+  attr_accessor :brand, :color, :size, :material
+  # attr_reader :material
   
   def initialize(brand)
     @brand = brand
-    @material = material
   end
-  
-  def material
-    @material
-  end
+
   
 end
 shoe = Shoe.new("Adidas")
-shoe.material
