1b3e9f121e5b1abcc33f9ce63566a03ab936ee1a
Ryna Truong <rynatruong@outlook.com>
2020-02-29 05:45:40 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,16 @@
-# Make your shoe class here!
+class shoe
+  attr_accessor :brand
+  attr_reader :
+  
+  def initialize(brand)
+    @brand = brand
+  end
+  
+  def brand(name)= name
+    
+  def brand
+    @shoe
+  end
+  
+end
+shoe = Shoe.new("Adidas")
