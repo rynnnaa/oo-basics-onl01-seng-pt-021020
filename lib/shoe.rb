d8d0e77fa82beeb3f0fceaa28b3cd4c2b983bff8
Ryna Truong <rynatruong@outlook.com>
2020-02-29 05:46:21 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,15 +1,9 @@
 class shoe
-  attr_accessor :bran
-  attr_reader :brand
+  attr_accessor :brand
+  # attr_reader :brand
   
   def initialize(brand)
     @brand = brand
-  end
-  
-  def brand(name)= name
-    
-  def brand
-    @shoe
   end
   
 end
